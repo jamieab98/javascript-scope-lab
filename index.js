745b9990f2532d8d4b59facddad7b216df3ef515
Jamie Bryson <jamieab98@gmail.com>
2025-08-14 16:50:35 -0400

    created the if true statement

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 // Write your solution in this file!
 const burgers = ["Hamburger", "Cheeseburger"];
+
 let featuredDrink = "Strawberry Milkshake";
+
 function addBurger() {
     const newBurger = "Flatburger";
     burgers.push(newBurger);
+}
+
+if (true) {
+    const anotherNewBurger = "Maple Bacon Burger";
+    burgers.push(anotherNewBurger);
 }
 
 console.log(burgers);
