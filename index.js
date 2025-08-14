441654da35bc1175b65bcff01872396963a15eb2
Jamie Bryson <jamieab98@gmail.com>
2025-08-14 16:34:05 -0400

    created the bugers array, the featuredDrink variable, and the addBurger function

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution in this file!
+const burgers = ["Hamburger", "Cheeseburger"];
+let featuredDrink = "Strawberry Milkshake";
+function addBurger() {
+    const newBurger = "Flatburger";
+    burgers.push(newBurger);
+}
+
+console.log(burgers);
+addBurger();
+console.log(burgers);
+console.log(featuredDrink)
